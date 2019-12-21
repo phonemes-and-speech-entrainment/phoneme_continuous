91838ac18b0f0cd930669def0fa12d431b0392c3
Oana Cucu <mc14237@bristol.ac.uk>
2019-12-21 15:28:55 +0200

    Update pow_coh2.m

--- Updated code/pow_coh2.m
+++ Updated code/pow_coh2.m
@@ -1,3 +1,5 @@
+%calculates power coherence (see Luo and Poeppel, 2007) between EEG trials 
+
 function C_pow=pow_coh2(eegspects)
 
 v=[1,2,3,4];
@@ -12,11 +14,11 @@ for i=1:size(C_pow,1)
         for l=1:size(C_pow,3)
             for h=1:size(C_pow,4)
                 for g=1:size(C_pow,5)
-                    eegpow1=eegspects(i,j,l,A(h,1),g,:);
-                    eegpow2=eegspects(i,j,l,A(h,2),g,:);
-                    Amp=abs(eegpow1.*conj(eegpow2));
+                    eegpow1=eegspects(i,j,l,A(h,1),g,:);   %spectrogram of EEG trial 1
+                    eegpow2=eegspects(i,j,l,A(h,2),g,:);   %spectrogram of EEG trial 2
+                    Amp=abs(eegpow1.*conj(eegpow2));       %magnitude of the resultant vector between the two trials
                     meanamp=mean(Amp,6);
-                    C_pow(i,j,l,h,g)=sqrt(mean((Amp.^2-meanamp.^2).^2,6))./meanamp.^2;
+                    C_pow(i,j,l,h,g)=sqrt(mean((Amp.^2-meanamp.^2).^2,6))./meanamp.^2;  %power coherence between trials 1 and 2
                 end
              end
          end
