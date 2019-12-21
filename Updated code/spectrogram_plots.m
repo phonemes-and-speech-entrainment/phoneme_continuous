f892568bc8c1cf82c81631e3a57416f837d8aa75
Oana Cucu <mc14237@bristol.ac.uk>
2019-12-21 15:37:18 +0200

    Update spectrogram_plots.m

--- Updated code/spectrogram_plots.m
+++ Updated code/spectrogram_plots.m
@@ -1,21 +1,26 @@
+ %spectrograms of an EEG trial and corresponding stimulus, plotted separately
+   
    clearvars;
 
    load('Spectrograms_without_500ms.mat');
    load('Soundspects_no_envelope.mat');
 
+   %first extract lengths of both sound file and EEG spectrograms to see if they correspond
+   %sound file
    z = squeeze(Soundspects(22,:,:));
    t = 0.375:0.01:1.125;
    x = 1:50;
    surf(t,x,abs(z),'EdgeColor','none');   
    axis xy; axis tight; colormap(jet); view(0,90);
    
+   %EEG
    z2 = squeeze(Spects(14,4,1,1,:,:));
    t2 = 0.375:0.01:1.125;
    x2 = 1:50;
    surf(t2,x2,abs(z2),'EdgeColor','none');   
    axis xy; axis tight; colormap(jet); view(0,90);
    
-   
+%spectrogram of the first EEG trial to sound file 1, for participant 2, at channel 4  
 spectrogram(squeeze(Data(2,4,:,1,1)),hann(nsc),650,F,1000,'yaxis')
 xlabel('Time (seconds)','FontSize',13,'FontWeight','bold');
 ylabel('Frequency (Hz)','FontSize',13,'FontWeight','bold');
@@ -25,6 +30,7 @@ c = colorbar;
 c.Label.FontSize = 12;
 c.Label.FontWeight = 'bold';
 
+%spectrogram of sound file 1
 spectrogram(X(1,:),hann(nsc),650,F,1000,'yaxis');
 xlabel('Time (seconds)','FontSize',13,'FontWeight','bold');
 ylabel('Frequency (Hz)','FontSize',13,'FontWeight','bold');
